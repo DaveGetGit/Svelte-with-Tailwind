3dd1a35a52593c978c65b63b43e34f89c74a804e
daveget2020@gmail.com <daveget2020@gmail.com>
2024-05-24 15:52:16 +0200

    feat(app): adding components and include them into the main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script>
 	import '../app.css';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+
+	let y;
+	let innerHeight = 0;
+	let innerWidth = 0;
+
+	function goTop() {
+		//window.scrollTo({ top: 0, behavior: 'smooth' });
+		document.body.scrollIntoView();
+	}
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+	<div
+		class={'fixed bottom-0 w-full duration-200 p-10 z-[10]' + y > 0
+			? 'opacity-full pointer-events-auto'
+			: 'pointer-events-none opacity-0'}
+	>
+		<button><i class="fa-solid fa-arrow-up"></i></button>
+	</div>
+	<Header {y} />
+	<slot />
+	<Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
