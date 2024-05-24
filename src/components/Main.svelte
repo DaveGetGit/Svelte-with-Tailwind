4761dc1cb3009ac1b2242322b7778dbf3aff5747
daveget2020@gmail.com <daveget2020@gmail.com>
2024-05-24 16:19:17 +0200

    fix(app): fix grid layout for projects

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -50,7 +50,7 @@
 
 	<section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
 		<div class="flex flex-col gap-2 text-center">
-			<h6 class="text-large sm:textxl md:text-2xl">Here are a few of my creative endeavors</h6>
+			<h6 class="text-large sm:text-xl md:text-2xl">Here are a few of my creative endeavors</h6>
 			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
 				Curious to <span class="poppins text-violet-400">see</span> my
 				<span class="poppins text-violet-400">work</span>?
@@ -59,8 +59,8 @@
 		<a
 			href="#"
 			target="_blank"
-			class="mx-auto px-4 py-2 rounded-md border border-solid border-white items-center gap-2 -mb-4
-                sm:mb-0 -mt-10 hover:border-violet-700 duration-200"
+			class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4
+                sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
 		>
 			<i class="fa-regular fa-circle-play"></i>
 			<p>Watch the video</p>
@@ -74,8 +74,6 @@
 					>ffsdf faf ew ewdsd wEeWE.
 				</p>
 			</Step>
-		</div>
-		<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
 			<Step step={steps[1]}>
 				<p>
 					kkjckh dhkjh fwedcs wefewf wefewf with <strong class="text-violet-400"
@@ -83,8 +81,6 @@
 					>ffsdf faf ew ewdsd wEeWE.
 				</p>
 			</Step>
-		</div>
-		<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
 			<Step step={steps[2]}>
 				<p>
 					kkjckh dhkjh fwedcs wefewf wefewf with <strong class="text-violet-400"
@@ -99,6 +95,14 @@
 		id="about"
 		class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
 	>
-		<div class="flex flex-col gap-2 text-center relative"></div>
+		<div
+			class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700
+            after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4 sm:py-6 md:py-8"
+		>
+			<h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+			<h3 class="font-semibold text-3xl sm:text4xl md:text-5xl">
+				A bit <span class="poppins text-violet-400">about</span> me.
+			</h3>
+		</div>
 	</section>
 </main>
